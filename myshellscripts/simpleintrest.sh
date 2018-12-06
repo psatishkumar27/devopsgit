7d552b65659adf74fa8011f33752e64936120465
psatishkumar27 <37478817+psatishkumar27@users.noreply.github.com>
2018-12-06 10:05:46 -0600

    Update simpleintrest.sh

--- myshellscripts/simpleintrest.sh
+++ myshellscripts/simpleintrest.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-echo "Enter the Principle Amount: "
-read p
-echo "Enter the rate of interest: "
-read r
-echo "Enter the number of years: "
-read n
+p=$1
+r=$2
+n=$3
 
 i=`expr $p \* $r \* $n`
 i=`expr $i / 100`
